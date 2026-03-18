7ff5221136338dafd9dfb8fc884ad78e2b3ad573
Adithya Gowda <duroviaai@gmail.com>
2026-03-18 16:17:34 +0530

    Day-8 latest

---
+++ Day-8/Project-10/Result/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import PassFail from "./components/PassFail";
+
+function App() {
+    return (
+        <div>
+            <PassFail />
+        </div>
+    );
+}
+export default App;
